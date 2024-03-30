8c47ec3d5a5d33a62679bff364913885d7196e8f
Guillaume Flambard <g.flambard@gmail.com>
2024-03-30 20:12:33 +0100

    fix/useFetch-case[id]

--- pages/cases/[id].vue
+++ pages/cases/[id].vue
@@ -8,30 +8,30 @@
       <div class="loader absolute"></div>
     </div> -->
 
-    <article v-for="c in casesData" :key="c.id_cas">
+    <article>
       <div>
         <div class="flex flex-col gap-3">
-          <h2 class="font-bold text-xl">{{ c.cas_nom_dossier }}</h2>
+            <h2 class="font-bold text-xl">{{ data?.cas_nom_dossier }}</h2>
           <div class="flex flex-wrap justify-between">
             <div class="mx-2">
-              <p>📍 {{ c.cas_zone_nom }}</p>
-              <p>👀 {{ c.cas_public }}</p>
+              <p>📍 {{ data?.cas_zone_nom }}</p>
+              <p>👀 {{ data?.cas_public }}</p>
             </div>
             <div class="mx-2">
-              <p>👤 {{ c.cas_temoignages_nb }}</p>
-              <p>🎖️ {{ c.cas_classification_calc }}</p>
+              <p>👤 {{ data?.cas_temoignages_nb }}</p>
+              <p>🎖️ {{ data?.cas_classification_calc }}</p>
             </div>
           </div>
-          <p>🗓️ {{ c.cas_AAAA }}</p>
+          <p>🗓️ {{ data?.cas_AAAA }}</p>
           <div class="flex flex-col gap-6 my-2">
             <p class="text-sm">
               <span class="font-bold">Témoignage officiel :</span><br />
-              <p class="leading-10" v-html="c.cas_resume_web" />
+              <p class="leading-10" v-html="data?.cas_resume_web" />
             </p>
             <p class="text-sm">
               <span class="font-bold">Conclusion pro :</span><br />
 
-              <p class="leading-10" v-html="c.cas_resume" />
+              <p class="leading-10" v-html="data?.cas_resume" />
             </p>
           </div>
         </div>
@@ -41,55 +41,14 @@
 </template>
 
 <script setup lang="ts">
-import Papa from "papaparse";
+import type { CaseData } from "~/types/cases";
 
-// Définition de l'interface pour les données des cas
-interface CaseData {
-	id_cas: string;
-	cas_nom_dossier: string;
-	cas_zone_nom: string;
-	cas_public: string;
-	cas_temoignages_nb: string;
-	cas_classification_calc: string;
-	cas_AAAA: string;
-	cas_resume_web: string;
-	cas_resume: string;
-}
-const loading = ref(false);
-// Utilisation de useRoute pour accéder aux paramètres de route
-const route = useRoute();
-const id = ref(route.params.id as string);
+const { id } = useRoute().params;
 
-// Déclaration de la référence pour stocker les données des cas
-const casesData = ref<CaseData[]>([]);
-// Fonction pour charger et parser le fichier CSV
-const loadCasesData = () => {
-	const csvUrl = "/public_cases.csv";
-	loading.value = true;
-	fetch(csvUrl)
-		.then((response) => response.text())
-
-		.then((csvText) => {
-			Papa.parse(csvText, {
-				header: true,
-				skipEmptyLines: true,
-				complete: (results) => {
-					// Filtrage des données en fonction de l'ID et assurance du type via une assertion
-					const parsedData = results.data as CaseData[];
-					casesData.value = parsedData.filter(
-						(c): c is CaseData => c.id_cas === id.value,
-					);
-					loading.value = false;
-				},
-			});
-		})
-		.catch((error) =>
-			console.error("Erreur lors du chargement du fichier CSV:", error),
-		);
-};
-
-// Exécution de la fonction loadCasesData au montage du composant
-onMounted(loadCasesData);
+const { data: fetchData, pending: loading } = await useFetch(
+	`/api/cases/${id}`,
+);
+const data = ref<CaseData | null>(fetchData.value ? fetchData.value[0] : null);
 </script>
 
 
---
+++ server/api/cases/[id].ts
@@ -0,0 +1,27 @@
+import Papa from 'papaparse';
+import type { CaseData } from '~/types/cases';
+
+export default defineEventHandler(async (event) => {
+    const id = getRouterParam(event, 'id');
+    // URL vers votre fichier CSV dans le dossier `static`
+    const csvUrl = 'http://localhost:3000/public_cases.csv';
+
+    // Utilisez $fetch pour récupérer le contenu CSV directement comme texte
+    const csvData: string = await $fetch(csvUrl);
+
+    // Convertissez CSV en JSON
+    return new Promise((resolve, reject) => {
+        Papa.parse(csvData, {
+            header: true,
+            skipEmptyLines: "greedy", // Mis à jour pour correspondre à la documentation de PapaParse
+            complete: (results) => {
+                console.log(results.data.filter((row: unknown) => (row as CaseData).id_cas === id));
+
+                resolve(results.data.filter((row: unknown) => (row as CaseData).id_cas === id));
+            },
+            error: (error: Error) => {
+                reject(error);
+            }
+        });
+    });
+});
