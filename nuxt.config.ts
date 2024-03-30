ccc8e69f2b70dbd3a305bb7645091689106523c1
Guillaume Flambard <g.flambard@gmail.com>
2024-03-30 22:25:10 +0100

    added/mapBox

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui',
     '@nuxtjs/tailwindcss',
-    'nuxt-icon'],
+    'nuxt-icon', 'nuxt-mapbox'],
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoiem9hbmxvZ2lhIiwiYSI6ImNsdWVsNzZhazBiZXEya3JvdzY1NnRkcXkifQ.SBSKPBqL7eT_feWhQBupUQ'
+  },
   css: ['~/layouts/global.css'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
--- pages/map/index.vue
+++ pages/map/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <h1 class="font-extrabold text-3xl text-center">Map</h1>
+    <div class="h-screen flex items-center justify-center relative">
+
+      <MapboxMap
+      map-id="{ID}"
+      class="absolute inset-0 w-full h-full rounded-full"
+      :options="{
+        style: 'mapbox://styles/zoanlogia/cluelezpp009501ntb28m254a', // style URL
+        center: [-68.137343, 45.137451], // starting position
+        zoom: 1.5 // starting zoom
+      }"
+    />
+  </div>
+
   </div>
 </template>
 
